4407637df251a621688822c5308ff564a30878e5
112-mariem-mansour <mansourmariemti12@gmail.com>
2023-12-04 13:20:59 +0100

    news-letter

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin', {
-    relativeSourceLocation: true,
-},],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
